300ae478b3ed49eea6828980621ac7c033252665
Daniel Correia <130774958+DanielJavaprogrammer@users.noreply.github.com>
2025-04-26 12:18:13 -0300

    Landing Page
    Finalizado

--- logic.js
+++ logic.js
@@ -1,21 +1,20 @@
 //Abrir Menu
-document.querySelector('.abrir_menu').addEventListener("click", ()=>{
-    const menu = document.querySelector(".menu_mobile");
-    if(menu.style.right != '0px'){
+document.querySelector('.abrir_menu').addEventListener("click", () => {
+    const menu = document.querySelector("#menu_mobile");
+    if (menu.style.right != '0px') {
         menu.style.right = '0px'
     }
 })
 
 //Fechar Menu
-document.querySelector(".fechar_menu").addEventListener("click", () =>{
-    const fechar_menu = document.querySelector(".menu_mobile");
+document.querySelector(".fechar_menu").addEventListener("click", () => {
+    const fechar_menu = document.querySelector("#menu_mobile");
     fechar_menu.style.right = '-1000px'
 })
 
 //Fechar menu ao redimensionar a tela
 window.addEventListener("resize", () => {
     if (window.innerWidth > 765) {
-      document.querySelector(".menu_mobile").style.right = "-1000px";
+        document.querySelector(".menu_mobile").style.right = "-1000px";
     }
-  });
-  
+});
